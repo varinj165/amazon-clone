6ea592f6eae397fe6cccb84964e342f5a10d4bfc
Varin <admin@Varins-MacBook-Pro-2.local>
2022-08-07 21:05:45 -0700

    Implemented initial Header code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header.js'
+import Home from './Home.js'
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyD7wgd6e6i3TCTylwQZMaL0St9wXyZt-rs",
+    authDomain: "clone-d3ee4.firebaseapp.com",
+    projectId: "clone-d3ee4",
+    storageBucket: "clone-d3ee4.appspot.com",
+    messagingSenderId: "115346196436",
+    appId: "1:115346196436:web:35bf9016a321d1663f3841",
+    measurementId: "G-CNV97QKZCZ"
+  };
