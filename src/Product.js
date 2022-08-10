2315ad9447e6f24a6b2d6c589a7d3a7a3af637f4
Varin <admin@Varins-MacBook-Pro-2.local>
2022-08-09 21:06:01 -0700

    Worked more on the product component styling

--- src/Product.js
+++ src/Product.js
@@ -5,13 +5,21 @@ function Product() {
   return (
     <div className='product'>
       <div className='product__info'>
-        <p>The lean startup</p>
+        <p>Dyson V8 Absolute Vacuum</p>
         <p className='product__price'>
             <small>$</small>
             {/** Make price bold */}
-            <strong>19.99</strong>
+            <strong>399.99</strong>
         </p>
+
+        <div className='product__rating'>
+          <p>⭐️</p>
+        </div>
       </div>
+
+      <img src='https://dyson-h.assetsadobe2.com/is/image/content/dam/dyson/images/products/primary/214730-01.png?$responsive$&cropPathE=mobile&fit=stretch,1&wid=640' alt=''/>
+
+      <button>Add to Basket</button>
     </div>
   )
 }
