6cc7b15cd69600ce29c5af370d0bf576faaec71b
Varin <admin@Varins-MacBook-Pro-2.local>
2022-10-26 23:45:25 -0700

    Starting firebase authentication

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault(); // Will help it not refresh when this function is called
+
+    // Firebase login functionality
+  }
+
+  const register = e => {
+    e.preventDefault();
+
+    // Firebase register functionality
+  }
+
   return (
     <div className='login'>
       <Link to='/'>
@@ -22,21 +37,26 @@ function Login() {
             <h5>
                 Email
             </h5>
-            <input type='text'/>
+            <input type='text' value={email} onChange= 
+            {e => setEmail(e.target.value)} />
 
             <h5>
                 Password
             </h5>
-            <input type='password'/>
+            <input type='password' value={password}
+            onChange={e => setPassword(e.target.value)}
+            />
 
-            <button className='login__signInButton'>Sign In</button>
+            <button type='submit' onClick={signIn}
+            className='login__signInButton'>Sign In</button>
 
             <p>
                 By signing-in you agree to this FAKE AMAZON CLONE'S Conditions of Use & Sale. Please
                 see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
             </p>
 
-            <button className='login__registerButton'>Create your Amazon account</button>
+            <button onClick={register} 
+            className='login__registerButton'>Create your Amazon account</button>
         </form>
       </div>
     </div>
